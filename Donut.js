2c73348d927638039deaffcfb3b4b5aea8078c8a
Yhancik <yhancik@gmail.com>
2019-07-31 12:46:27 +0200

    setHole fait

--- Donut.js
+++ Donut.js
@@ -1,29 +1,51 @@
-class Donut(){
+const c = 0.55191502449;
+
+class Donut{
     
-    constructor(x, y, rad, w, col, rune){
+    constructor(x, y, rad, h, col, rune){
         this.x = x;
         this.y = y;
         this.rad = rad;
-        this.w = w;
+        this.h = h;
         this.col = col;
         this.rune = rune;
         
-        this.c = 0.55191502449;
-        
-        this.shape = = rune.path(x, y)
+        this.shape = rune.path(x, y)
             .moveTo(0,1)
             .curveTo(c, 1, 1, c, 1, 0)
             .curveTo(1, -c, c, -1, 0, -1)
             .curveTo(-c, -1, -1, -c, -1, 0)
             .curveTo(-1, c, -c, 1, 0, 1)
             .closePath()
-            .moveTo(0*w,1*w)
-            .curveTo(c*w, 1*w, 1*w, c*w, 1*w, 0*w)
-            .curveTo(1*w, -c*w, c*w, -1*w, 0*w, -1*w)
-            .curveTo(-c*w, -1*w, -1*w, -c*w, -1*w, 0*w)
-            .curveTo(-1*w, c*w, -c*w, 1*w, 0*w, 1*w)
-            .fillRule("evenodd").fill("#c0e300").scale(200).stroke(false);
+            .moveTo(0*h,1*h)
+            .curveTo(c*h, 1*h, 1*h, c*h, 1*h, 0*h)
+            .curveTo(1*h, -c*h, c*h, -1*h, 0*h, -1*h)
+            .curveTo(-c*h, -1*h, -1*h, -c*h, -1*h, 0*h)
+            .curveTo(-1*h, c*h, -c*h, 1*h, 0*h, 1*h)
+            .fillRule("evenodd").fill(col).scale(rad).stroke(false);
         
+        console.log(this.shape);
+    }
+    
+    setHole(h){
+        this.shape.scale(1/this.rad);
+        
+        this.shape.state.anchors[6] = new Rune.Anchor()
+            .setMove(0*h,1*h);
+        
+        this.shape.state.anchors[7] = new Rune.Anchor()
+            .setCurve(c*h, 1*h, 1*h, c*h, 1*h, 0*h);
+        
+        this.shape.state.anchors[8] = new Rune.Anchor()
+            .setCurve(1*h, -c*h, c*h, -1*h, 0*h, -1*h);
+        
+        this.shape.state.anchors[9] = new Rune.Anchor()
+            .setCurve(-c*h, -1*h, -1*h, -c*h, -1*h, 0*h);
+        
+        this.shape.state.anchors[10] = new Rune.Anchor()
+            .setCurve(-1*h, c*h, -c*h, 1*h, 0*h, 1*h);
+
+        this.shape.scale(this.rad);
     }
     
 }
--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var r = new Rune({
     debug: false
 });
 
-c = 0.55191502449;
+/*c = 0.55191502449;
 p = 0.5;
 
 var don = r.path(0, 666)
@@ -20,13 +20,19 @@ var don = r.path(0, 666)
     .curveTo(1*p, -c*p, c*p, -1*p, 0*p, -1*p)
     .curveTo(-c*p, -1*p, -1*p, -c*p, -1*p, 0*p)
     .curveTo(-1*p, c*p, -c*p, 1*p, 0*p, 1*p)
-  .fillRule("evenodd").fill("#c0e300").scale(200).stroke(false);
+  .fillRule("evenodd").fill("#c0e300").scale(200).stroke(false);*/
 
-console.log(don);
+var don = new Donut(333,200,120,0.5,"#c0e300",r);
+
+//console.log(don);
+don.setHole(0.1);
+
+var frameCount = 0;
 
 r.on('update', function() {
-  don.move(0.1, 0, true);
-    
+  don.shape.move(0.1, 0, true);
+  don.setHole(Math.sin(frameCount*0.01));  
+    frameCount++;
 });
 
 r.play()
