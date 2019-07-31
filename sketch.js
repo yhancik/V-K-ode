131b9f7e07d948865fab3bef92aed33825854f95
Yhancik <yhancik@gmail.com>
2019-07-31 11:24:52 +0200

    Donut solved!

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var r = new Rune({
     container: "body",
-    width: 888,
-    height: 666,
-    //debug: true
+    width: 1200,
+    height: 1200,
+    debug: false
 });
 
 /*var svg = new Rune.Svg('svg/wshop.svg');
@@ -33,5 +33,51 @@ svg.load(function(err) {
 
 console.log("après le svg.load");*/
 
-var form = new Form("svg/wshop.svg","#058795", r);
-form.draw();
+//var form = new Form("svg/wshop.svg","#058795", r);
+//form.draw();
+
+/*r.path(0, 0)
+    .curveTo(100, 0, 0, 0, 50, 0)
+    .curveTo(100, 100, 100, 0, 100, 50)
+    .curveTo(0, 100, 100, 100, 50, 100)
+    .curveTo(0, 0, 0, 100, 0, 50)
+  .closePath().scale(5)*/
+  /*.moveTo(50, 50)
+  .lineTo(140, 50)
+  .lineTo(140, 140)
+  .lineTo(50, 140)
+  .closePath()
+  .fillRule("evenodd");*/;
+
+
+
+r.path(222, 222)
+    .moveTo(0,-200)
+    .lineTo(200, 0)
+    .lineTo(0, 200)
+    .lineTo(-200, 0)
+    .lineTo(0, -200)
+  .fill("#efdddd").strokeWidth(5)
+;
+
+c = 0.55191502449;
+p = 0.5;
+
+r.path(666, 666)
+    .moveTo(0,1)
+    .curveTo(c, 1, 1, c, 1, 0)
+    .curveTo(1, -c, c, -1, 0, -1)
+    .curveTo(-c, -1, -1, -c, -1, 0)
+    .curveTo(-1, c, -c, 1, 0, 1)
+    .closePath()
+    .moveTo(0*p,1*p)
+    .curveTo(c*p, 1*p, 1*p, c*p, 1*p, 0*p)
+    .curveTo(1*p, -c*p, c*p, -1*p, 0*p, -1*p)
+    .curveTo(-c*p, -1*p, -1*p, -c*p, -1*p, 0*p)
+    .curveTo(-1*p, c*p, -c*p, 1*p, 0*p, 1*p)
+  .fillRule("evenodd").fill("#c0e300").scale(200).stroke(false);
+
+r.circle(400,400,12).fill("#ebff00").stroke(false);
+
+
+r.draw();
